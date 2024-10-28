879c7530a2d3ada82778ebe74e5be560c35c582d
RoBaertschi <rtmbaertschi007@gmail.com>
2024-10-28 08:36:28 +0100

    Use the real template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { LoggerMiddleware } from './sample/midleware/logger.middleware';
+import { ArticleModule } from './sample/modules/article/article.module';
+import { AuthModule } from './sample/modules/auth/auth.module';
+import { RootModule } from './sample/modules/root/root.module';
+import { ResetModule } from './sample/modules/reset/reset.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ArticleModule,
+    AuthModule,
+    RootModule,
+    ResetModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'better-sqlite3',
+        database: process.env.DATABASE_NAME || 'database/api.db',
+        dropSchema: true,
+        entities: [],
+        autoLoadEntities: true,
+        logging: process.env.DATABASE_LOG?.toLowerCase() === 'true' || false,
+        // todo: achtung nicht benutzen in der Produktion
+        synchronize: true,
+      }),
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes();
+  }
+}
